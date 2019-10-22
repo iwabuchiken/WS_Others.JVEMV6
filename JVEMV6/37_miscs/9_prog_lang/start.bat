d8e1caab6586aea9aec95c923bf96567fbe44dc0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-10-22 16:47:55 +0900

    (WS_Others.JVEMV6)
    periodical commit
    on occation of : res birds
    ----------------------------
    g c -a
    
    # Changes to be committed:
    #	modified:   JVEMV6/37_miscs/9_prog_lang/ancient_greek.ods
    #	modified:   JVEMV6/37_miscs/9_prog_lang/ancient_hebrew.odt
    #	modified:   JVEMV6/37_miscs/9_prog_lang/start.bat
    #	new file:   JVEMV6/37_miscs/9_prog_lang/turkish.odt
    #	new file:   "JVEMV6/41_economics/7_topics/2_\345\233\275\346\260\221\345\201\245\345\272\267\344\277\235\351\231\272/41_7.2_1.odt"

--- JVEMV6/37_miscs/9_prog_lang/start.bat
+++ JVEMV6/37_miscs/9_prog_lang/start.bat
@@ -48,7 +48,7 @@ REM : text
 REM 	start launcher.exe "http://www.qbible.com/hebrew-old-testament/ecclesiastes/2.html"
 REM 	start launcher.exe "http://www.qbible.com/hebrew-old-testament/ecclesiastes/3.html"
 REM 	start launcher.exe "http://www.qbible.com/hebrew-old-testament/isaiah/2.html#1"
-	start launcher.exe "http://www.qbible.com/hebrew-old-testament/micah/2.html#1"
+	start launcher.exe "http://www.qbible.com/hebrew-old-testament/micah/3.html#1"
 REM : wiki
 	start launcher.exe "https://ja.wikipedia.org/wiki/ƒwƒuƒ‰ƒC•¶Žš#•¶Žš"
 REM : pronunciations
