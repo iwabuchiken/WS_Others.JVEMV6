883e6c4846ffbe071e1c2b294f820cd065625c7f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-12-13 10:30:59 +0900

    (res : religions) periodical updates

--- JVEMV6/37_miscs/9_prog_lang/start.bat
+++ JVEMV6/37_miscs/9_prog_lang/start.bat
@@ -50,7 +50,8 @@ REM : text
 REM 	start brave.exe "http://www.qbible.com/hebrew-old-testament/ecclesiastes/2.html"
 REM 	start brave.exe "http://www.qbible.com/hebrew-old-testament/ecclesiastes/3.html"
 REM 	start brave.exe "http://www.qbible.com/hebrew-old-testament/isaiah/2.html#1"
-	start launcher.exe "http://www.qbible.com/hebrew-old-testament/micah/7.html#1"
+start launcher.exe "http://www.qbible.com/hebrew-old-testament/nahum/1.html"
+REM 	start launcher.exe "http://www.qbible.com/hebrew-old-testament/micah/7.html#1"
 REM : wiki
 	start launcher.exe "https://ja.wikipedia.org/wiki/ƒwƒuƒ‰ƒC•¶Žš#•¶Žš"
 REM : pronunciations
