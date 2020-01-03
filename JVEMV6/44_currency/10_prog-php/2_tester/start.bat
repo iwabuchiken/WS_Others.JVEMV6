12dce8d38a7bd1c2615d9d69990d2f3a55c4e220
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-01-03 16:37:26 +0900

    res free# JVEMV6 44#10.2.1_XX / 44. currency / 10. prog-php / 2. tester / 1. start /  20200103_160636

---
+++ JVEMV6/44_currency/10_prog-php/2_tester/start.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM opening message
+echo starting start.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\3_fx-log-file
+
+REM =================================
+REM 	log file
+REM 	
+REM =================================
+
+REM =================================
+REM 	log file
+REM 	2019/12/24 14:43:23
+REM =================================
+pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\3_fx-log-file
+start log_JVE_37_12_3.[fx-log-file].odt
+
+
+:end_of_end
+
+pause
