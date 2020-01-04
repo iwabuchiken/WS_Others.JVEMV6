710264798c66c28ed39c501cadefe216e7d150f9
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-01-04 16:35:52 +0900

    res free# JVEMV6 44#10.2_2 / 44. currency / 10. prog-php / 2. tester /  20200104_153103

--- JVEMV6/44_currency/10_prog-php/2_tester/start.bat
+++ JVEMV6/44_currency/10_prog-php/2_tester/start.bat
@@ -1,20 +1,34 @@
+REM =================================
+REM 	log file
+REM 	git
+
+REM =================================
 @echo off
 
 REM opening message
-echo starting start.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\3_fx-log-file
+echo starting start.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\44_currency\10_prog-php\2_tester
 
 REM =================================
 REM 	log file
-REM 	
+REM 	2020/01/04 15:21:38
 REM =================================
+pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\44_currency\10_prog-php\2_tester
+start log_JVE_44_10_2.[fx-php-tester].odt
 
 REM =================================
-REM 	log file
-REM 	2019/12/24 14:43:23
+REM 	eclipse
+REM 	2020/01/04 15:21:38
 REM =================================
-pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\3_fx-log-file
-start log_JVE_37_12_3.[fx-log-file].odt
+pushd C:\WORKS_2\Programs\eclipse\eclipse_luna-php,python
+start eclipse.exe
 
+REM =================================
+REM 	git
+REM 	2020/01/04 15:21:38
+REM =================================
+REM C:\WORKS_2\a.bat
+C:\WORKS_2\batches\r.bat oj && C:\WORKS_2\batches\s.bat && C:\WORKS_2\batches\r.bat c && C:\WORKS_2\batches\s.bat
+REM  && r c && s
 
 :end_of_end
 
