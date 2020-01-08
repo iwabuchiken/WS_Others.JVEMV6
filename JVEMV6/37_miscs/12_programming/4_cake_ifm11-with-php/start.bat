369b79e3bda38ac28eb52729461dea98cac0477d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-01-08 10:49:35 +0900

    res free# JVEMV6 37#12.4_1 / 37. miscs / 12. programming / 4. cake_ifm11-with-php /  20200108_094400

---
+++ JVEMV6/37_miscs/12_programming/4_cake_ifm11-with-php/start.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM opening message
+echo starting start.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\4_cake_ifm11-with-php
+
+REM =================================
+REM 	log file
+REM 	
+REM =================================
+
+REM =================================
+REM 	log file
+REM 	2019/12/24 14:43:23
+REM =================================
+pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\4_cake_ifm11-with-php
+start log_JVE_37_12_4.[cake_ifm11-with-php].odt
+
+
+:end_of_end
+
+pause
