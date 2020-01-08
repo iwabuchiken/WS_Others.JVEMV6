c5203dceca5f3066ba313f66b92cfe86d948d742
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-01-08 14:29:15 +0900

    res free# JVEMV6 37#12.4_2 / 37. miscs / 12. programming / 4. cake_ifm11-with-php /  20200108_130621

--- JVEMV6/37_miscs/12_programming/4_cake_ifm11-with-php/start.bat
+++ JVEMV6/37_miscs/12_programming/4_cake_ifm11-with-php/start.bat
@@ -5,7 +5,9 @@ echo starting start.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_pro
 
 REM =================================
 REM 	log file
-REM 	
+REM 	eclipse
+REM 	git
+REM 	browser
 REM =================================
 
 REM =================================
@@ -15,6 +17,31 @@ REM =================================
 pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\4_cake_ifm11-with-php
 start log_JVE_37_12_4.[cake_ifm11-with-php].odt
 
+REM =================================
+REM 	eclipse
+REM 	2020/01/08 13:01:27
+REM =================================
+REM pushd C:\WORKS_2\Programs\eclipse\eclipse_luna-php,python
+pushd C:\WORKS_2\Programs\eclipse\eclipse_luna_php
+start eclipse.exe
+
+REM =================================
+REM 	git
+REM 	2020/01/08 13:01:27
+REM =================================
+REM C:\WORKS_2\a.bat
+C:\WORKS_2\batches\r.bat oj && C:\WORKS_2\batches\s.bat && C:\WORKS_2\batches\r.bat c && C:\WORKS_2\batches\s.bat
+REM  && r c && s
+
+REM =================================
+REM 	browser
+REM 	2020/01/08 13:01:27
+REM =================================
+set url_1=http://localhost/Eclipse_Luna/Cake_IFM11/ifm
+
+pushd C:\WORKS_2\Programs\opera
+REM launcher.exe http://localhost/Eclipse_Luna/Cake_IFM11/ifm
+launcher.exe %url_1%
 
 :end_of_end
 
