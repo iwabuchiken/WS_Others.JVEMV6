269e220ccd89bf2789724d912db2c0775f34537a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-01-14 13:31:02 +0900

    res free# JVEMV6 44#10.3.1_5 / 44. currency / 10. prog-php / 3. admin / 1. fx-log-file /  20200114_125627

---
+++ JVEMV6/44_currency/10_prog-php/3_admin/1_fx-log-file/end_apps.bat
@@ -0,0 +1,6 @@
+taskkill /im wish.exe
+taskkill /im sakura.exe
+
+taskkill /im eclipse.exe
+
+pause
---
+++ JVEMV6/44_currency/10_prog-php/3_admin/1_fx-log-file/start.bat
@@ -0,0 +1,46 @@
+@echo off
+
+REM opening message
+echo starting start.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\12_programming\3_fx-log-file
+
+REM =================================
+REM 	log file
+REM 	eclipse
+REM 	git
+REM =================================
+
+REM =================================
+REM 	log file
+REM 	2020/01/14 12:51:58
+REM =================================
+pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\44_currency\10_prog-php\3_admin\1_fx-log-file
+start log_JVE_44_10.3_1.[fx-log-file].odt
+
+REM =================================
+REM 	eclipse
+REM 	2020/01/14 12:52:41
+REM =================================
+REM pushd C:\WORKS_2\Programs\eclipse\eclipse_luna-php,python
+pushd C:\WORKS_2\Programs\eclipse\eclipse_luna_php
+start eclipse.exe
+
+REM =================================
+REM 	browser
+REM 	2020/01/14 12:53:14
+REM =================================
+pushd C:\WORKS_2\Programs\opera
+launcher.exe http://localhost/Eclipse_Luna/Cake_IFM11/fx_admin
+
+
+REM =================================
+REM 	git
+REM 	2020/01/14 12:53:20
+REM =================================
+REM C:\WORKS_2\a.bat
+C:\WORKS_2\batches\r.bat oj && C:\WORKS_2\batches\s.bat && C:\WORKS_2\batches\r.bat c && C:\WORKS_2\batches\s.bat
+
+
+
+:end_of_end
+
+pause
