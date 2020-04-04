7575995955bede6b7832c08d9ec176d5497ed49f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-04-04 14:09:08 +0900

    res free# JVEMV6 44#10.2_ 56_1 / 44. currency / 10. prog-php / 2. tester /  56. prog:php:utils / 20200404_120501

--- JVEMV6/41_economics/7_topics/end_Glass-Steagall.[41#7.5].bat
+++ JVEMV6/41_economics/7_topics/end_Glass-Steagall.[41#7.5].bat
@@ -1,1 +1,6 @@
+echo.>> C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_log_JVE_41.[economics].bat
+
+C:\WORKS_2\a && pushd C:\Users\iwabuchiken\Desktop\shortcuts_docs && git add . && git commit -m "(shortcuts : res : start_log_JVE_41.[economics].bat) periodical updates" && e && git push origin master
+
+
 C:\WORKS_2\a && r oj && git add . && git commit -m "(JVEMV6:41_econ:7_topics:5) periodical updates" && e && git push origin master
