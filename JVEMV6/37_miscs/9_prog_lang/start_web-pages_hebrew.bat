307e9e968d9025d558778e1d0d159777e15baa92
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-05-21 14:34:32 +0900

    (lang : hebrew) periodical updates

--- JVEMV6/37_miscs/9_prog_lang/start_web-pages_hebrew.bat
+++ JVEMV6/37_miscs/9_prog_lang/start_web-pages_hebrew.bat
@@ -5,9 +5,9 @@ echo starting start_web-pages_hebrew.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV
 
 pushd "C:\WORKS_2\Programs\opera"
 
-set url_1="http://www.qbible.com/hebrew-old-testament/proverbs/6.html#1"
-set url_2=https://www.scripture4all.org/OnlineInterlinear/OTpdf/pro6.pdf
-set url_3=https://www.mechon-mamre.org/p/pt/pt2806.htm
+set url_1="http://www.qbible.com/hebrew-old-testament/proverbs/7.html#1"
+set url_2=https://www.scripture4all.org/OnlineInterlinear/OTpdf/pro7.pdf
+set url_3=https://www.mechon-mamre.org/p/pt/pt2807.htm
 set dictionary=https://www.thefreedictionary.com
 set time_calc=http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
 set stopwatch=https://stopwatch-app.com
