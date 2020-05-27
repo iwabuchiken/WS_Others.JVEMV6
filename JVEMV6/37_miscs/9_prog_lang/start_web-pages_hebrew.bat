d4cf4836d73ff7c078212e750da66ed18900a6cf
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-05-27 10:36:57 +0900

    [1] res free# JVEMV6 44#10.2.2_XX / 44. currency / 10. prog-php / 2. tester / 2.  tester_1__V2 / 20200527_102234

--- JVEMV6/37_miscs/9_prog_lang/start_web-pages_hebrew.bat
+++ JVEMV6/37_miscs/9_prog_lang/start_web-pages_hebrew.bat
@@ -5,8 +5,8 @@ echo starting start_web-pages_hebrew.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV
 
 pushd "C:\WORKS_2\Programs\opera"
 
-set url_1="http://www.qbible.com/hebrew-old-testament/proverbs/8.html#1"
-set url_2=https://www.scripture4all.org/OnlineInterlinear/OTpdf/pro8.pdf
+set url_1=https://www.scripture4all.org/OnlineInterlinear/OTpdf/pro8.pdf
+set url_2="http://www.qbible.com/hebrew-old-testament/proverbs/8.html#1"
 set url_3=https://www.mechon-mamre.org/p/pt/pt2808.htm
 set dictionary=https://www.thefreedictionary.com
 set time_calc=http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
