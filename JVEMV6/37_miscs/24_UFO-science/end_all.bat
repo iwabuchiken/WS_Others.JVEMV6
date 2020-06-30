d331b358953afb0723162dabbcb967f2fc52ed7d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-06-30 19:04:00 +0900

    [5] res free# JVEMV6 37#24.1_X / 37. miscs / 24. UFO-science / 1. eman / 20200630_181753

---
+++ JVEMV6/37_miscs/24_UFO-science/end_all.bat
@@ -0,0 +1,56 @@
+@echo off
+
+echo param is '%1'
+
+echo all params are : '%*'
+
+set param=%1
+
+REM ==================================
+REM 	validate : param ?
+REM ==================================
+rem if "%param%"=='' (
+if "%param%"=="" (
+
+	echo param is blank
+	
+	goto end
+)
+
+REM ==================================
+REM 	git push : C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11
+REM ==================================
+
+rem @echo on
+
+echo setting param...
+
+rem set param="%1"
+set param="%*"
+
+
+echo executing...
+C:\WORKS_2\a.bat && r oj && git add -A && git commit -m %param% && e && p && echo.>> C:\WORKS_2\shortcuts_docs\start_log_JVE_37#24.[UFO-science].bat
+
+goto end
+
+REM ==================================
+REM 	closing
+REM ==================================
+rem echo.>> C:\WORKS_2\shortcuts_docs\start_log_JVE_44_10_2.[fx-php-tester].bat
+
+rem goto end
+
+taskkill /im wish.exe
+taskkill /im sakura.exe
+
+taskkill /im eclipse.exe
+
+taskkill /im metaeditor.exe
+
+echo.>> C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_log_JVE_44_10_2.[fx-php-tester].bat
+
+
+:end
+
+pause
