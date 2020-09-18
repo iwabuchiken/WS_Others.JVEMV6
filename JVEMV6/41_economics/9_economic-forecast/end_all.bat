f9fd5f30ad6873dce94ca17e5da1dd8fd53d2e3b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-09-18 22:07:33 +0900

    [15] res free# JVEMV6 37#23.1_X / 37. miscs / 23. CO2 / 1. mind-map / 20200918_171608

---
+++ JVEMV6/41_economics/9_economic-forecast/end_all.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo param is '%1'
+
+echo all params are : '%*'
+
+set param=%1
+
+REM ==================================
+REM 	validate : param ?
+REM ==================================
+rem if "%param%"=='' (
+if "%param%"=="" (
+
+	echo param is blank
+	
+	goto end
+)
+
+REM ==================================
+REM 	git push : C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11
+REM ==================================
+
+rem @echo on
+
+echo setting param...
+
+rem set param="%1"
+set param="%*"
+
+
+echo executing...
+rem C:\WORKS_2\a.bat && r oj && git add -A && git commit -m %param% && e && p && echo.>> C:\WORKS_2\shortcuts_docs\start_log_JVE_37#23.[CO2].bat
+C:\WORKS_2\a.bat && r oj && git add -A && git commit -m %param% && e && p
+
+:end
+
+pause
