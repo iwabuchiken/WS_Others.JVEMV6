8aeb327ca113dea578aebfd9ee7a22f7f98d132d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-02-22 09:06:24 +0900

    (lang : hebrew) periodical updates

--- JVEMV6/37_miscs/9_prog_lang/start_web-pages_hebrew.bat
+++ JVEMV6/37_miscs/9_prog_lang/start_web-pages_hebrew.bat
@@ -1,23 +1,28 @@
 @echo off
 
+rem =========================
 REM opening message
+rem =========================
 echo starting start_web-pages_hebrew.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang
 
 pushd "C:\WORKS_2\Programs\opera"
 
-set url_1=https://www.scripture4all.org/OnlineInterlinear/OTpdf/pro15.pdf
-set url_2="http://www.qbible.com/hebrew-old-testament/proverbs/15.html#1"
-set url_3=https://www.mechon-mamre.org/p/pt/pt2815.htm
+rem : resource : https://scripture4all.org/OnlineInterlinear/Hebrew_Index.htm	: 2021年2月22日8:48:53
+set url_1=https://scripture4all.org/OnlineInterlinear/OTpdf/qoh1.pdf
+set url_2="http://www.qbible.com/hebrew-old-testament/ecclesiastes/1.html#4"
+set url_3=https://www.mechon-mamre.org/p/pt/pt3101.htm
 
 rem : add : 2021年2月8日9:09:07
-set url_4=http://www.mechon-mamre.org/p/p0000.htm
+rem : c/o : 2021年2月22日8:45:49
+rem set url_4=http://www.mechon-mamre.org/p/p0000.htm
 
 rem : other sites : 2021年2月8日9:22:24
 rem https://www.abarim-publications.com/Dictionary/a/a-ta.html
 
-set dictionary=https://www.thefreedictionary.com
-set time_calc=http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
-set stopwatch=https://stopwatch-app.com
+rem : c/o : 2021年2月22日8:46:05
+rem set dictionary=https://www.thefreedictionary.com
+rem set time_calc=http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+rem set stopwatch=https://stopwatch-app.com
 
 set trans_1="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ko"
 set trans_2="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=zh-CN"
