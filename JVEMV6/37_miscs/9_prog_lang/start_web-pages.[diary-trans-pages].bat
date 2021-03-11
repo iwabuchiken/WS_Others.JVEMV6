7751fc38b76e36ec34d3acf9a2bfaea877a77fd5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-03-11 15:34:49 +0900

    (myself : vietnamese) periodical updates

--- JVEMV6/37_miscs/9_prog_lang/start_web-pages.[diary-trans-pages].bat
+++ JVEMV6/37_miscs/9_prog_lang/start_web-pages.[diary-trans-pages].bat
@@ -5,21 +5,24 @@ echo starting start_web-pages.[diary-trans-pages].bat // C:\WORKS_2\WS\WS_Others
 
 pushd "C:\WORKS_2\Programs\opera"
 
-set trans_1_1="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=pl&tl=en"
-set trans_1_2="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=pl"
+set trans_pl_en="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=pl&tl=en"
+set trans_en_pl="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=pl"
 
-set trans_2_1="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=ru&tl=en"
-set trans_2_2="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ru"
+set trans_ru_en="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=ru&tl=en"
+set trans_en_ru="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ru"
 
-set trans_3_1="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=ar&tl=en"
-set trans_3_2="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ar"
+set trans_ar_en="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=ar&tl=en"
+set trans_en_ar="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ar"
 
-set trans_4_1="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=zh-CN&tl=en"
-set trans_4_2="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=zh-CN"
+set trans_zhcn_en="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=zh-CN&tl=en"
+set trans_en_zhcn="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=zh-CN"
 
-rem %trans_1_1% %trans_1_2% %trans_2_1% %trans_2_2% %trans_3_1% %trans_3_2% %trans_4_1% %trans_4_2%
+rem %trans_pl_en% %trans_en_pl% %trans_ru_en% %trans_en_ru% %trans_ar_en% %trans_en_ar% %trans_zhcn_en% %trans_en_zhcn%
 
-start launcher.exe %trans_1_1% %trans_1_2% %trans_2_1% %trans_2_2% %trans_3_1% %trans_3_2% %trans_4_1% %trans_4_2%
+start launcher.exe %trans_pl_en% %trans_en_pl%
+start launcher.exe  %trans_ru_en% %trans_en_ru%
+start launcher.exe  %trans_ar_en% %trans_en_ar%
+start launcher.exe  %trans_zhcn_en% %trans_en_zhcn%
 
 REM :end_of_end
 
