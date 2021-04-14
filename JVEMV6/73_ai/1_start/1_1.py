722b94a5e51cfe2b22564bcde8890462484cd9d5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-04-14 16:06:06 +0900

    (JVE_73.[theme=ai]) periodical updates

--- JVEMV6/73_ai/1_start/1_1.py
+++ JVEMV6/73_ai/1_start/1_1.py
@@ -43,6 +43,7 @@ import sys
 # from sympy.solvers.tests.test_constantsimp import C2
 
 import os
+from pandas.tests.io.msgpack.test_case import test_9
 
 sys.path.append('.')
 sys.path.append('..')
@@ -72,10 +73,13 @@ from shutil import copyfile
 # import math
 
 ###############################################
-import matplotlib.pyplot as plt, matplotlib.patches as pat
-import math, numpy
+import matplotlib.pyplot as plt, matplotlib.patches as pat, math, numpy
 
 from scipy import stats
+
+#ref https://www.w3schools.com/python/python_ml_polynomial_regression.asp
+from sklearn.metrics import r2_score
+
 
 def test_1_statistics():
 
@@ -172,7 +176,7 @@ def test_3_stats_Percentile():
                 , libs.get_TimeLabel_Now()
                 )
     )
-    #aaa
+    
     #code:20210411_171845
     ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
     
@@ -571,6 +575,285 @@ def test_7_Linear_Regression():
     
 # def test_7_Linear_Regression()://
     
+def test_8_Regression_Poly():
+
+    strOf_FuncName = "test_8_Regression_Poly"
+
+
+    '''###################
+        step : 1
+            opening, vars
+    ###################'''
+    print()
+    
+    print ("[%s:%d] starting : %s (time=%s)" % (
+                os.path.basename(os.path.basename(libs.thisfile()))
+                , libs.linenum()
+                , strOf_FuncName
+                , libs.get_TimeLabel_Now()
+                )
+    )
+    
+    '''###################
+        step : 2
+            histo
+    ###################'''
+    x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+    y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+    
+    #code:20210414_152625
+#     mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+    valOf_Fit   = 5
+    mymodel = numpy.poly1d(numpy.polyfit(x, y, valOf_Fit))
+
+    myline = numpy.linspace(1, 22, 100)
+    
+#     #debug
+#     print ("[%s:%d] myline =>" % 
+#                 (
+#                  os.path.basename(libs.thisfile())
+#                  , libs.linenum()
+#                  )
+#            )
+#     
+#     print(myline)
+
+    fig = plt.figure()
+    
+    plt.scatter(x, y)
+    
+    plt.plot(myline, mymodel(myline))
+    
+#     mymodel = list(map(myfunc, x))
+#     plt.plot(x, mymodel)
+    
+    '''###################
+        step : 3
+            plot
+    ###################'''
+    dpath_PlotImage = "./data"
+    fname_PlotImage = "plot_image_%s" % (libs.get_TimeLabel_Now())
+    
+    fpath_PlotImage = os.path.join(dpath_PlotImage, fname_PlotImage)
+    
+    # title
+#     plt.title("scatter : x, y" 
+    plt.title("scatter : x, y\nvalOf_Fit : %d" 
+            % (
+               valOf_Fit
+                )
+              
+              
+              )
+    
+    fig.savefig(fpath_PlotImage)
+
+    # message
+    print ("[%s:%d] save fig => done : %s" % 
+            (
+             os.path.basename(libs.thisfile())
+             , libs.linenum()
+             , fpath_PlotImage
+             )
+           
+           
+           )
+
+    '''###################
+        step : 2
+    ###################'''
+    
+# def test_8_Regression_Poly()://
+    
+def test_9_R_Square():
+
+    strOf_FuncName = "test_9_R_Square"
+
+
+    '''###################
+        step : 1
+            opening, vars
+    ###################'''
+    print()
+    
+    print ("[%s:%d] starting : %s (time=%s)" % (
+                os.path.basename(os.path.basename(libs.thisfile()))
+                , libs.linenum()
+                , strOf_FuncName
+                , libs.get_TimeLabel_Now()
+                )
+    )
+    
+    '''###################
+        step : 2
+            histo
+    ###################'''
+    x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+    y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+    
+    #code:20210414_152625
+#     mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+    valOf_Fit   = 5
+    mymodel = numpy.poly1d(numpy.polyfit(x, y, valOf_Fit))
+
+    print ("[%s:%d] r2_score(y, mymodel(x)) ==>" 
+#            % 
+#             (
+#              os.path.basename(libs.thisfile())
+#              , libs.linenum()
+#              , fpath_PlotImage
+#              )
+            
+            
+           )
+    print(r2_score(y, mymodel(x)))
+    
+#     fig = plt.figure()
+#     
+#     plt.scatter(x, y)
+#     
+#     plt.plot(myline, mymodel(myline))
+#     
+# #     mymodel = list(map(myfunc, x))
+# #     plt.plot(x, mymodel)
+#     
+#     '''###################
+#         step : 3
+#             plot
+#     ###################'''
+#     dpath_PlotImage = "./data"
+#     fname_PlotImage = "plot_image_%s" % (libs.get_TimeLabel_Now())
+#     
+#     fpath_PlotImage = os.path.join(dpath_PlotImage, fname_PlotImage)
+#     
+#     # title
+# #     plt.title("scatter : x, y" 
+#     plt.title("scatter : x, y\nvalOf_Fit : %d" 
+#             % (
+#                valOf_Fit
+#                 )
+#               
+#               
+#               )
+#     
+#     fig.savefig(fpath_PlotImage)
+# 
+#     # message
+#     print ("[%s:%d] save fig => done : %s" % 
+#             (
+#              os.path.basename(libs.thisfile())
+#              , libs.linenum()
+#              , fpath_PlotImage
+#              )
+#            
+#            
+#            )
+
+    '''###################
+        step : 2
+    ###################'''
+    
+# def test_9_R_Square()://
+    
+def test_10_Predict():
+
+    strOf_FuncName = "test_10_Predict"
+
+
+    '''###################
+        step : 1
+            opening, vars
+    ###################'''
+    print()
+    
+    print ("[%s:%d] starting : %s (time=%s)" % (
+                os.path.basename(os.path.basename(libs.thisfile()))
+                , libs.linenum()
+                , strOf_FuncName
+                , libs.get_TimeLabel_Now()
+                )
+    )
+    
+    '''###################
+        step : 2
+            histo
+    ###################'''
+    x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+    y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+    
+    #code:20210414_152625
+#     mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+    valOf_Fit   = 5
+    mymodel = numpy.poly1d(numpy.polyfit(x, y, valOf_Fit))
+    
+    myline = numpy.linspace(1, 22, 100)
+    
+#     print ("[%s:%d] r2_score(y, mymodel(x)) ==>" 
+# #            % 
+# #             (
+# #              os.path.basename(libs.thisfile())
+# #              , libs.linenum()
+# #              , fpath_PlotImage
+# #              )
+#             
+#             
+#            )
+#     print(r2_score(y, mymodel(x)))
+    
+    fig = plt.figure()
+     
+    plt.scatter(x, y)
+     
+    plt.plot(myline, mymodel(myline))
+    
+    # prediction
+    numOf_Prediction_Target = 17
+    
+    #ref https://stackoverflow.com/questions/16006572/plotting-different-colors-in-matplotlib
+    #ref https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html <== this
+    plt.plot(numOf_Prediction_Target, mymodel(numOf_Prediction_Target), 'ro')
+#     plt.plot(numOf_Prediction_Target, mymodel(numOf_Prediction_Target), col='red')
+    
+     
+    '''###################
+        step : 3
+            plot
+    ###################'''
+    dpath_PlotImage = "./data"
+    fname_PlotImage = "plot_image_%s" % (libs.get_TimeLabel_Now())
+     
+    fpath_PlotImage = os.path.join(dpath_PlotImage, fname_PlotImage)
+     
+    # title
+#     plt.title("scatter : x, y" 
+    plt.title("scatter : x, y\nvalOf_Fit : %d\npredict-for : x = %d" 
+            % (
+               valOf_Fit
+               , numOf_Prediction_Target
+                )
+               
+               
+              )
+     
+    fig.savefig(fpath_PlotImage)
+ 
+    # message
+    print ("[%s:%d] save fig => done : %s" % 
+            (
+             os.path.basename(libs.thisfile())
+             , libs.linenum()
+             , fpath_PlotImage
+             )
+            
+            
+           )
+
+    '''###################
+        step : 2
+    ###################'''
+    
+# def test_10_Predict()://
+    
     
 
 def exec_prog(): # from : 
@@ -582,7 +865,11 @@ def exec_prog(): # from :
     #n:20210413_142903
     #n:20210412_151638
     #n:20210411_172354
-    test_7_Linear_Regression()
+    #n:20210414_160053
+    test_10_Predict()
+#     test_9_R_Square()
+#     test_8_Regression_Poly()
+#     test_7_Linear_Regression()
 #     test_6_plot_Scatter()
 #     test_5_stats_Normal_Distribution()
 #     test_4_stats_Distribution()
@@ -630,3 +917,37 @@ if __name__ == "__main__" :
     
     print ("[%s:%d] all done" % (os.path.basename(os.path.basename(libs.thisfile())), libs.linenum()))
 
+
+
+
+'''###################
+    playarea
+
+20210414_151340
+#ref https://numpy.org/doc/stable/reference/generated/numpy.poly1d.html
+import numpy as np
+p = np.poly1d([1, 2, 3])
+print(p)
+           2
+        1 x + 2 x + 3
+p(1.5)
+        8.25
+p.r
+        array([-1.+1.41421356j, -1.-1.41421356j])
+        
+print(p.r)
+        [-1.+1.41421356j -1.-1.41421356j]
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+pf = np.polyfit(x, y, 3)
+print(pf)
+        [ -3.03208795e-02   1.34333191e+00  -1.55383039e+01   1.13768037e+02]
+        
+mymodel = np.poly1d(pf)
+                  3         2
+        -0.03032 x + 1.343 x - 15.54 x + 113.8
+
+
+###################'''
