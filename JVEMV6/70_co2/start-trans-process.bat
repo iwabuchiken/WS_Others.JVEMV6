39c3f3c900e4b55c0f91806dd764608a71df525f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-05-16 16:22:06 +0900

    (JVE_41.[theme=economics:topic=12_economy-watch]) periodical updates

--- JVEMV6/70_co2/start-trans-process.bat
+++ JVEMV6/70_co2/start-trans-process.bat
@@ -1,2 +1,2 @@
 pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\app\Lib\others\jve_70-CO2_68_trans-string\
-echo =============================== >> log_process-trans-sentences.log && C:\xampp_5.6\php\php.exe process-trans-sentences.php >> log_process-trans-sentences.log && echo. >> log_process-trans-sentences.log
+echo =============================== >> log_process-trans-sentences.log && C:\xampp_5.6\php\php.exe process-trans-sentences.php >> log_process-trans-sentences.log && echo. >> log_process-trans-sentences.log && exit
