789aa1b6ba6aa39da15a3e9118562fac2543c1e8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-05-18 12:52:18 +0900

    (JVE_70.[theme=CO2]) periodical updates

---
+++ JVEMV6/70_co2/start_[process-texts-for-reading_korean].bat
@@ -0,0 +1,5 @@
+pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\app\Lib\others\jve_70-CO2_68_trans-string\
+echo =============================== >> log_process-texts-for-reading_korean.log && C:\xampp_5.6\php\php.exe process-texts-for-reading_korean.php >> log_process-texts-for-reading_korean.log && echo. >> log_process-texts-for-reading_korean.log
+
+
+rem pause
