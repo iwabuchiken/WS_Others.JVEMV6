2facd17897571d7dcf03fb56163b52b9e9e75f3e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-05-20 17:06:00 +0900

    (JVE_70.[theme=CO2]) periodical updates

--- JVEMV6/70_co2/start-trans-process_chinese.bat
+++ JVEMV6/70_co2/start-trans-process_chinese.bat
@@ -1,2 +1,4 @@
 pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\app\Lib\others\jve_70-CO2_68_trans-string\
 echo =============================== >> log_process-trans-sentences_chinese.log && C:\xampp_5.6\php\php.exe process-trans-sentences_chinese.php >> log_process-trans-sentences_chinese.log && echo. >> log_process-trans-sentences_chinese.log && exit
+rem echo =============================== >> log_process-trans-sentences_chinese.log && C:\xampp_5.6\php\php.exe process-trans-sentences_chinese.php >> log_process-trans-sentences_chinese.log && echo. >> log_process-trans-sentences_chinese.log
+
