0417cd199351461e119ecbebd6b87c15d377093b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-06-06 10:36:33 +0900

    (JVE_75.[science-research]) periodical updates

--- JVEMV6/70_co2/start_process-texts-for-reading_chinese.bat
+++ JVEMV6/70_co2/start_process-texts-for-reading_chinese.bat
@@ -1,5 +1,5 @@
 pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\app\Lib\others\jve_70-CO2_68_trans-string\
 echo =============================== >> log_process-texts-for-reading_chinese.log && C:\xampp_5.6\php\php.exe process-texts-for-reading_chinese.php >> log_process-texts-for-reading_chinese.log && echo. >> log_process-texts-for-reading_chinese.log
 
-
+exit
 rem pause
