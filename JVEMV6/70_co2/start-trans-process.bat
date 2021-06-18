25915def7d63192e728a8a0588847da227804a55
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-06-18 11:46:38 +0900

    (JVE_72.[read]) periodical updates

--- JVEMV6/70_co2/start-trans-process.bat
+++ JVEMV6/70_co2/start-trans-process.bat
@@ -1,2 +1,8 @@
 pushd C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\app\Lib\others\jve_70-CO2_68_trans-string\
-echo =============================== >> log_process-trans-sentences.log && C:\xampp_5.6\php\php.exe process-trans-sentences.php >> log_process-trans-sentences.log && echo. >> log_process-trans-sentences.log && exit
+rem echo =============================== >> log_process-trans-sentences.log && C:\xampp_5.6\php\php.exe process-trans-sentences.php >> log_process-trans-sentences.log && echo. >> log_process-trans-sentences.log && exit
+
+echo =============================== >> log_process-trans-sentences.log && C:\xampp_5.6\php\php.exe process-trans-sentences.php >> log_process-trans-sentences.log && echo. >> log_process-trans-sentences.log
+
+rem pause
+
+rem sakura C:\WORKS_2\WS\Eclipse_Luna\Cake_IFM11\app\Lib\others\jve_70-CO2_68_trans-string\process-trans-sentences.php
