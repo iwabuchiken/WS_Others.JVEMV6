1d4617379a4a7ee27752205b2cff34aa33e811d6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-02-17 10:52:42 +0900

    (lang : hebrew) periodical updates

--- JVEMV6/37_miscs/9_prog_lang/storage_9_prog_lang/start_web-pages_hebrew.bat
+++ JVEMV6/37_miscs/9_prog_lang/storage_9_prog_lang/start_web-pages_hebrew.bat
@@ -5,7 +5,9 @@ REM opening message
 rem =========================
 echo starting start_web-pages_hebrew.bat // C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang
 
-pushd "C:\WORKS_2\Programs\opera"
+rem pushd "C:\WORKS_2\Programs\opera"
+pushd "C:\Program Files\Mozilla Firefox"
+
 
 rem : resource : https://scripture4all.org/OnlineInterlinear/Hebrew_Index.htm	: 2021年2月22日8:48:53
 rem set url_1=https://scripture4all.org/OnlineInterlinear/OTpdf/qoh1.pdf
@@ -16,7 +18,12 @@ set url_1=https://scripture4all.org/OnlineInterlinear/OTpdf/pro26.pdf
 set url_2="http://www.qbible.com/hebrew-old-testament/proverbs/26.html#13"
 rem set url_2="http://www.qbible.com/hebrew-old-testament/genesis/1.html#1"
 
-set url_3=https://www.mechon-mamre.org/p/pt/pt0101.htm
+set url_3=https://mechon-mamre.org/p/pt/pt2826.htm
+
+
+set list_of_urls=%url_1% %url_2% %url_3%
+
+firefox.exe  %list_of_urls%
 
 rem : add : 2021年2月8日9:09:07
 rem : c/o : 2021年2月22日8:45:49
@@ -30,14 +37,19 @@ rem set dictionary=https://www.thefreedictionary.com
 rem set time_calc=http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
 rem set stopwatch=https://stopwatch-app.com
 
+rem 2022年2月17日10:43:17
+rem https://biblescripture.net/Hebrew.html
+
 rem : c/o : 2021年7月3日11:00:22
 rem set trans_1="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ko"
 rem set trans_2="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=zh-CN"
 
 rem start launcher.exe %url_1% %dictionary% %url_2% %url_3% %time_calc% %stopwatch% %trans_1% %trans_2%
 
-start launcher.exe %url_1% %dictionary% %url_2% %url_3% %url_4% %trans_1% %trans_2%
+rem start launcher.exe %url_1% %dictionary% %url_2% %url_3% %url_4% %trans_1% %trans_2%
 
 REM :end_of_end
 
 REM gen ==> time label
+
+exit
