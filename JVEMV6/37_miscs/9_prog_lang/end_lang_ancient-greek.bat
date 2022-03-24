a7e90083cc30f819c92ea90e527e191472bbfea0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-03-24 17:59:45 +0900

    (lang : ancient greek) periodical updates

---
+++ JVEMV6/37_miscs/9_prog_lang/end_lang_ancient-greek.bat
@@ -0,0 +1,25 @@
+@echo off
+
+REM =================================
+REM 	opening message
+REM 	2022”N3ŒŽ23“ú16:57:23
+REM =================================
+echo starting : C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang\end_lang_ancient-greek.bat
+
+
+
+REM =================================
+REM 	C:\WORKS_2\a.bat
+REM 	2022”N3ŒŽ23“ú16:57:23
+REM =================================
+C:\WORKS_2\a.bat && r oj && git add -A && git commit -m "(lang : ancient greek) periodical updates" && e && p
+
+
+REM =================================
+REM 	git
+REM 	2022”N3ŒŽ23“ú16:57:23
+REM =================================
+rem r oj && git add -A && git commit -m "(lang : hebrew) periodical updates" && e && p
+
+pause
+
