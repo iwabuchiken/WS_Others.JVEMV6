16d40faaa8df8b710d391006e9b8fb3865a86d55
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-03-02 17:23:33 +0900

    JVE_72.[read] : bk:243_sosuu

---
+++ JVEMV6/57_programming/57.5_random_id_manager/jve_57.5_main.php
@@ -0,0 +1,46 @@
+<?php 
+
+	/***
+	 * 
+
+file			C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\57_programming\57.4_change_file_name\s-6\jve_57_g-5_s-6.php
+started at		20221025_105206
+
+
+pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\57_programming\57.5_random_id_manager\
+php jve_57.5_main.php
+
+echo.>> log_g-5_s-6.log && echo ============================ >> log_g-5_s-6.log && C:\WORKS_2\t.bat >> log_g-5_s-6.log && echo.>> log_g-5_s-6.log && C:\xampp_5.6\php\php.exe jve_57_g-5_s-6.php >> log_g-5_s-6.log
+//echo.>> log_g-5_s-6.log && echo ============================ >> log_g-5_s-6.log && C:\WORKS_2\t.bat >> log_g-5_s-6.log && echo.>> log_g-5_s-6.log && jve_57_g-5_s-6.py >> log_g-5_s-6.log
+
+
+pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\57_programming\57.4_change_file_name\s-6\
+php jve_57_g-5_s-6.php
+
+	 */
+	function main_exec() {
+		
+		/******************** (20 '*'s)
+		 * step : 1 : read file
+		 ********************/
+		printf("[%s : %d] main_exec --> started", basename(__FILE__), __LINE__);
+		
+		
+		
+		
+	}//function main_exec() {
+	
+// 	echo "yes";
+	
+// 	printf("[%s : %d] yes", __FILE__, __LINE__);
+// 	echo "\n";
+	
+// 	printf("[%s : %d] yeeeees", basename(__FILE__), __LINE__);
+// 	echo "\n";
+
+	/******************** (20 '*'s)
+	* execute
+	********************/
+	main_exec();
+	
+?>
